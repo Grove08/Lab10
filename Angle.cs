b2a4a0931b08b6e58b26957fda45a07bcafa9783
user <user@WIN-2MSGKLHNS9A>
2023-12-26 10:38:49 +0300

    Добавьте файлы проекта.

---
+++ Angle.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    internal class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public bool isCorrect = true;
+        
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set
+            {
+                if (value >= 0 && value < 360)
+                    gradus = value;
+                else
+                    isCorrect = false;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                if (value >= 0 && value < 60)
+                    min = value;
+                else
+                {
+                    Gradus += value / 60;
+                    min = value % 60;
+                }
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                if (value >= 0 && value < 60)
+                    sec = value;
+                else
+                {
+                    Min += value / 60;
+                    sec = value % 60;
+                }
+            }
+        }
+
+        public Angle(int gradus, int min, int sec)
+        {
+            this.Gradus = gradus;
+            this.Min = min;
+            this.Sec = sec;
+        }
+        public double ToRadians()
+        {
+            return (gradus + (min / 60.0) + (sec / 3600.0)) * Math.PI / 180.0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle;
+            do
+            {
+                Console.WriteLine("Введите данные");
+                int gradus_ = Convert.ToInt32(Console.ReadLine());
+                int min_ = Convert.ToInt32(Console.ReadLine());
+                int sec_ = Convert.ToInt32(Console.ReadLine());
+
+                angle = new Angle(gradus_, min_, sec_);
+            }
+            while (angle.isCorrect == false);
+
+            double radians = angle.ToRadians();
+            Console.WriteLine(radians);
+            Console.ReadKey();
+        }
+
+    }
+}
